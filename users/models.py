df6f2ecf59b7d6ed77dca0c051c6f1ef7033c866
mattkim <mattkim@mattkim-N.local>
2021-07-16 22:32:45 +0900

    make Room app

--- users/models.py
+++ users/models.py
@@ -38,3 +38,5 @@ class User(AbstractUser):
     language = models.CharField(choices=LANGUAGE_CHOICES, max_length=10, blank=True)
     currency = models.CharField(choices=CURRENCY_CHOICES, max_length=10, blank=True)
     superhost = models.BooleanField(default=False)
+
+
