4879bc4b1c8f0b2573cc62868adcf70f3ff153f9
mattkim <mattkim@mattkim-N.local>
2021-07-18 20:49:33 +0900

    Room admin finish

--- rooms/admin.py
+++ rooms/admin.py
@@ -6,6 +6,14 @@ class ItemAdmin(admin.ModelAdmin):
 
     """ Item Admin Definition """
 
+    list_display = (
+        "name",
+        "used_by",
+    )
+
+    def used_by(self, obj):
+        return obj.rooms.count()
+
     pass
 
 
@@ -14,11 +22,92 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
-    pass
+    fieldsets=(
+        (
+            "Basic Info",{
+                "fields": (
+                    "name", "description", "country", "address", "price"
+                )
+            }
+        ),
+        (
+            "Times",{
+                "fields":(
+                    "check_in","check_out","instant_book"
+                )
+            }
+        ),
+        (
+            "Space",{
+                "fields": (
+                    "guests", "beds", "bedrooms", "baths"
+                )
+            }
+        ),
+        (
+            "More About the Space",{
+                "classes":("collapse",),
+                "fields":(
+                    "amenities","facilities","house_rules"
+                )
+            }
+        ),
+        (
+            "Last Detials",{
+                "fields":(
+                    "host",
+                )
+            }
+        ),
 
-@admin.register(models.Photo)
-class PhotoAdmin(admin.ModelAdmin):
 
-    """ Photo Admin Definition """
+    )
 
-    pass
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "beds",
+        "bedrooms",
+        "baths",
+        "guests",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+        "count_photos",
+    )
+
+    list_filter = (
+        "instant_book",
+        "host__gender",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "country",
+        "city",
+    )
+    search_fields = ('city',)
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules"
+    )
+
+    def count_amenities(self, obj):
+        return obj.amenities.count()
+
+    def count_photos(self, obj):
+        return obj.photos.count()
+
+    @admin.register(models.Photo)
+    class PhotoAdmin(admin.ModelAdmin):
+
+        """ Photo Admin Definition """
+
+        pass
