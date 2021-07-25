570fbecc5a8efd428ec186baf2bddd1e25f09aeb
mattkim <mattkim@mattkim-N.local>
2021-07-25 14:56:22 +0900

    Make Admin Detail Fin

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from . import models
 
 @admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
@@ -16,17 +17,22 @@ class ItemAdmin(admin.ModelAdmin):
 
     pass
 
+class PhotoInline(admin.TabularInline):
+
+    model = models.Photo
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
+    inlines = (PhotoInline,)
+
     fieldsets=(
         (
             "Basic Info",{
                 "fields": (
-                    "name", "description", "country", "address", "price"
+                    "name", "description", "country", "city", "address", "price"
                 )
             }
         ),
@@ -92,7 +98,10 @@ class RoomAdmin(admin.ModelAdmin):
         "country",
         "city",
     )
-    search_fields = ('city',)
+
+    raw_id_fields = ("host",)
+
+    search_fields = ("=city", "^host__username")
 
     filter_horizontal = (
         "amenities",
@@ -106,9 +115,14 @@ class RoomAdmin(admin.ModelAdmin):
     def count_photos(self, obj):
         return obj.photos.count()
 
-    @admin.register(models.Photo)
-    class PhotoAdmin(admin.ModelAdmin):
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
 
-        """ Photo Admin Definition """
+    """ Photo Admin Definition """
 
-        pass
+    list_display = ("__str__", "get_thumbnail")
+
+    def get_thumbnail(self, obj):
+        return mark_safe(f'<img width="50px" src="{obj.file.url}" />')
+
+    get_thumbnail.short_description = "Thumbnail"
